120bcb86a3fee2256df3b7518daeb282fdfaa51d
shashankgupta <shashank.gupta006@gmail.com>
2024-07-21 00:19:59 +0530

    ui change

--- testlib/src/main/java/com/shashank/testlib/components/Composables.kt
+++ testlib/src/main/java/com/shashank/testlib/components/Composables.kt
@@ -1,10 +1,13 @@
 package com.shashank.testlib.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 
 @Composable
 fun TextComposable() {
-    Text("Hello from Lib!!")
+  Box {
+      Text(text = "Hello Maven Local!")
+  }
 }
